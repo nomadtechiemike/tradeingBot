92630bae9fded1de8fa8ee36a5848ccc89c60bf8
TechieMike <29424378+nomadtechiemike@users.noreply.github.com>
2025-12-14 08:19:04 +0700

    tsconfig

--- packages/core/src/db.ts
+++ packages/core/src/db.ts
@@ -2,7 +2,7 @@ import { Pool } from 'pg';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const pool = new Pool({ connectionString: process.env.DATABASE_URL || '' });
 
 export async function query(text: string, params?: any[]) {
   const res = await pool.query(text, params);
